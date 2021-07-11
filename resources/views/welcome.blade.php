5b78f116ba4aebb30238913adc02f9fe527af440
jorge bernal <jorgebernal08041999@gmail.com>
2021-07-11 15:45:44 -0500

    prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Jorge
+                    Jorge2
                 </div>
 
                 <div class="links">
