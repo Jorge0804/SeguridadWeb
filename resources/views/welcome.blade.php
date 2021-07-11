872f3e66925be45fd4c17fea09d33f681702ef10
jorge bernal <jorgebernal08041999@gmail.com>
2021-07-11 16:25:58 -0500

    prueba c5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Jorge4
+                    Jorge5
                 </div>
 
                 <div class="links">
