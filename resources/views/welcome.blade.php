fb4bcf220d2a79602f1e43fea05cb8096d737c15
jorge bernal <jorgebernal08041999@gmail.com>
2021-07-11 17:00:16 -0500

    prueba c7

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Jorge6
+                    Jorge7
                 </div>
 
                 <div class="links">
