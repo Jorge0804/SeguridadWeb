17f8568b46a737c80f4a9c272384c7eb948d4af4
jorge bernal <jorgebernal08041999@gmail.com>
2021-07-13 07:28:28 -0500

    Jorge 15

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Jorge 14
+                    Jorge 15
                 </div>
 
                 <div class="links">
