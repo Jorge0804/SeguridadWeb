9bf476c36f321349bf1797ca86ac12c5aae91f6a
jorge bernal <jorgebernal08041999@gmail.com>
2021-07-28 11:14:17 -0500

    Prueba cookie v14

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Request;
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
